990fb7eb8bedf796989e28388c724425ed7e1d98
Niratama <mabodohu>
2024-11-26 12:37:33 +0900

    no message

--- ZombieTowerDefense/Assets/Script/PlayerControl/PlayerControl.cs
+++ ZombieTowerDefense/Assets/Script/PlayerControl/PlayerControl.cs
@@ -1,23 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
 public class PlayerControl : MonoBehaviour
 {
+    //----------------------------------------------
+    // マウス系処理
+
+    [SerializeField] private Transform player;
+    private Camera mainCamera;
+
+    private Vector3 currentPosition = Vector3.zero;
+
+    //----------------------------------------------
+    // キーボード
 
     private bool Press;
+
+    //----------------------------------------------
+    // Playerの保存
+    private BaseSoldier currentPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Press);
+        MousePosition();
+    }
+
+    private void MousePosition()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            var raycastHitList = Physics.RaycastAll(ray).ToList();
+
+            // 地面をRayでとる
+            var baseFieldHits = raycastHitList
+               .Where(hit => hit.collider.GetComponent<Field>() != null)
+               .ToList();
+
+            if (baseFieldHits.Any())
+            {
+                var distance = Vector3.Distance(mainCamera.transform.position, raycastHitList.First().point);
+                var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+
+                currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+                currentPosition.y = 0;
+            }
+
+            // 兵士をRayでとる
+            var SoldierHits = raycastHitList
+               .Where(hit => hit.collider.GetComponent<Soldier>() != null)
+               .ToList();
+
+            if(SoldierHits.Any()) 
+            {
+                currentPlayer = raycastHitList.First().collider.gameObject.GetComponent<BaseSoldier>();
+            }
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -28,6 +77,17 @@ public class PlayerControl : MonoBehaviour
         if (Press) { Press = false; }
         else       { Press = true;  }
 
+
+
         
+    }
+
+    void OnDrawGizmos()
+    {
+        if (currentPosition != Vector3.zero)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(currentPosition, 0.5f);
+        }
     }
 }
--- ZombieTowerDefense/Assets/Script/Soldier/BaseSoldier.cs
+++ ZombieTowerDefense/Assets/Script/Soldier/BaseSoldier.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseSoldier : MonoBehaviour
+public class BaseSoldier : MonoBehaviour , Soldier , SelectObject
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +15,13 @@ public class BaseSoldier : MonoBehaviour
     {
         
     }
+}
+
+interface Soldier
+{
+
+}
+interface SelectObject
+{
+
 }
